f0f6b5abba82e2584a07ae317124955e04c845f8
Petro Lesapiti <petrolesapiti@gmail.com>
2022-09-21 13:17:09 +0300

    Direct a page to point to the calculator UI

---
+++ src/pages/CalculatorUI.js
@@ -0,0 +1,9 @@
+import Calculator from "../components/Calculator";
+
+const CalculatorUI = () => (
+  <div className="container">
+    <h2>Let&apos;s do same maths!</h2>
+    <Calculator />
+  </div>
+);
+export default CalculatorUI;
